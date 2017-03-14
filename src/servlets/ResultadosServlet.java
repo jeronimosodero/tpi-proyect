6af8cf9cbfaa948c892490e581f3a9fbc44ef9ba
jerosode <jerosode@gmail.com>
2017-03-14 18:14:15 -0300

    nuevo

--- src/servlets/ResultadosServlet.java
+++ src/servlets/ResultadosServlet.java
@@ -1,9 +1,19 @@
 package servlets;
 
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -11,15 +21,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import classes.Conexion;
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.QueryFactory;
+import org.apache.jena.query.ResultSet;
+import org.apache.jena.sparql.engine.http.QueryEngineHTTP;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.parser.Parser;
+import org.jsoup.select.Elements;
+
 import classes.Metadata;
 import classes.OA;
 import classes.OAs;
-import classes.Request;
-import classes.Result;
-import classes.ServiceGenerator;
-import endpoints.Endpoint;
-import retrofit2.Call;
 
 /**
  * Servlet implementation class ResultadosServlet
@@ -34,14 +49,57 @@ public class ResultadosServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		req.setAttribute("oas", oas);
+		System.out.println("doget");
 		ServletContext sc = getServletContext();
-		RequestDispatcher rd = sc.getRequestDispatcher("/resultados.jsp");
+		RequestDispatcher rd = sc.getRequestDispatcher("/buscador.jsp");
+		
 		rd.forward(req,resp);
+		System.out.println("doget");
 	}
 
 	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		oas = new OAs();
+		String busqueda = req.getParameter("busqcampo");
+		
+		String[] keys = req.getParameterValues("parametros");
+		String[] values = req.getParameterValues("campo");
+		
+		
+		FileInputStream fis = new FileInputStream(new File("C:/Users/Jero/Downloads/example.xml"));
+		InputStreamReader inputStreamReader = new InputStreamReader((InputStream)fis, "UTF-8");
+		BufferedReader br = new BufferedReader(inputStreamReader);
+		String line;
+		StringBuilder sb = new StringBuilder();
+		while ((line = br.readLine()) != null) {
+		    sb.append(line);
+		}
+		Document doc = Jsoup.parse(sb.toString(), "UTF-8", Parser.xmlParser());
+		//Document doc = Jsoup.connect("https://rdu.unc.edu.ar/oai/request?verb=ListRecords&metadataPrefix=oai_dc").followRedirects(true).get();
+		//String query = "record:contains("+ busqueda + ")";
+		String query = "record";
+		Elements records = doc.select(query);
+		for(int i = 0;i<keys.length;i++){
+			query = "record:has(" + keys[i] + ":contains(" + values[i] + "))";
+			System.out.println(query);
+			records = records.select(query);
+		}
+		
+		ArrayList<OA> oasList = new ArrayList<OA>();
+		for(Element record: records){
+			OA oa = new OA();
+			oa.setAbst(record.select("dc|description").text());
+			oa.setAutor(record.select("dc|creator").text());
+			oa.setFecha(record.select("datestamp").text());
+			oa.setTitulo(record.select("dc|title").text());
+			oa.setUrl("http://" + record.select("identifier").text().substring(4).replace(":", "/"));
+			oasList.add(oa);
+		}
+		System.out.println(oasList.size());
+		oas.setOas(oasList);
+		
+		/*
 		oas = new OAs();
 		String key = "";
 		String value = "";
@@ -96,8 +154,20 @@ public class ResultadosServlet extends HttpServlet {
 				e.printStackTrace();
 			}	
 		}
+		*/
 
 		doGet(req, resp);
+	}
+	
+	public ResultSet executeQuery(String queryString) throws Exception {
+		 Query query = QueryFactory.create(queryString) ;
+		 String service = "";
+		 String apikey = "";
+		 QueryEngineHTTP qexec = QueryExecutionFactory.createServiceRequest(service, query);
+		 qexec.addParam("apikey", apikey);
+		 ResultSet results = qexec.execSelect() ;
+		 return results;
+
 	}
 
 }
