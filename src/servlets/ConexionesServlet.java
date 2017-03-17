d478e81ee0601fcb6d47b172dd2c18247b0a4652
Julian Ruiz Diaz <Julian Ruiz Diaz>
2017-03-16 20:29:46 -0700

    Cosechador agregado

--- src/servlets/ConexionesServlet.java
+++ src/servlets/ConexionesServlet.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
